4cc308e2c8a63c6941a0541765bf4918e44ef63d
saidboussebni7-afk <saidboussebni7@gmail.com>
2026-05-05 21:07:51 +0100

    Delete pages/api/admin/session.js

--- pages/api/admin/session.js
+++
@@ -1,6 +0,0 @@
-const { getAdminSession } = require('../../../lib/session');
-
-export default function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ ok: false, error: 'method_not_allowed' });
-  return res.status(200).json({ ok: !!getAdminSession(req) });
-};
