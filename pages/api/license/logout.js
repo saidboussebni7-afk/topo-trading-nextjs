3c0777a9cb691ba47c19d782e03922c2d9bc8f75
saidboussebni7-afk <saidboussebni7@gmail.com>
2026-05-05 21:09:20 +0100

    Delete pages/api/license/logout.js

--- pages/api/license/logout.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', [
-    'topo_license=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; Secure',
-    'topo_device=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; Secure'
-  ]);
-  return res.status(200).json({ ok: true });
-}
