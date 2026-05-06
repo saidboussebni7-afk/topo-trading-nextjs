ae6b3f17c862cdb60cdaa622d9d80211138c53fe
saidboussebni7-afk <saidboussebni7@gmail.com>
2026-05-06 04:00:30 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,32 +1,13 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  // منع bypass
   if (request.headers.has("x-middleware-subrequest")) {
     return new NextResponse("Forbidden", { status: 403 });
   }
 
-  const path = request.nextUrl.pathname;
-  const token = request.cookies.get("admin_token")?.value;
-
-  const isAdminPage = path.startsWith("/vip-admin-6d8f2a");
-  const isAdminApi = path.startsWith("/api/admin");
-
-  // حماية الأدمن
-  if ((isAdminPage || isAdminApi) && !token) {
-    if (path.startsWith("/api/")) {
-      return NextResponse.json(
-        { success: false, message: "Unauthorized" },
-        { status: 401 }
-      );
-    }
-
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
   const response = NextResponse.next();
+  const path = request.nextUrl.pathname;
 
-  // Security headers
   response.headers.set("X-Frame-Options", "DENY");
   response.headers.set("X-Content-Type-Options", "nosniff");
   response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
@@ -35,7 +16,6 @@ export function middleware(request) {
     "camera=(), microphone=(), geolocation=(), payment=()"
   );
 
-  // منع الكاش للأدمن و API و signals
   if (
     path.startsWith("/api/") ||
     path.includes("admin") ||
